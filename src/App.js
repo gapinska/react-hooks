6f525447f6a86d55c7bc8e14678bad150e421674
Monika Gapińska <monika.gapinska@lppsa.com>
2021-02-19 17:24:18 +0100

    useReducer with useContext

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import './App.css'
 import Counter from './components/Counter'
 import CounterFour from './components/CounterFour'
 import CounterThree from './components/CounterThree'
 import CounterTwo from './components/CounterTwo'
-import ComponentC from './components/contextComopnents/ComponentC'
 import CounterUseReducer from './components/CounterUseReducer'
 import CounterUseReducer1 from './components/CounterUseReducer1'
 import CounterUseReducer2 from './components/CounterUseReducer2'
+import ComponentA from './components/useReducerComponents/ComponentA'
+import ComponentB from './components/useReducerComponents/ComponentB'
+import ComponentC from './components/useReducerComponents/ComponentC'
 
 export const UserContext = React.createContext()
 export const ChannelContext = React.createContext()
+export const CountContext = React.createContext()
+
+const initialState = 0
+const reducer = (state, action) => {
+	switch (action) {
+		case 'increment':
+			return state + 1
+		case 'decrement':
+			return state - 1
+		case 'reset':
+			return initialState
+		default:
+			return state
+	}
+}
 
 function App() {
+	const [ count, dispatch ] = useReducer(reducer, initialState)
 	return (
-		<div className="App">
-			<Counter />
+		<CountContext.Provider value={{ countState: count, countDispatch: dispatch }}>
+			<div className="App">
+				{/* <Counter />
 			<CounterTwo />
 			<CounterThree />
 			<CounterFour />
@@ -26,8 +45,13 @@ function App() {
 			</UserContext.Provider>
 			<CounterUseReducer />
 			<CounterUseReducer1 />
-			<CounterUseReducer2 />
-		</div>
+			<CounterUseReducer2 /> */}
+				Count - {count}
+				<ComponentA />
+				<ComponentB />
+				<ComponentC />
+			</div>
+		</CountContext.Provider>
 	)
 }
 
